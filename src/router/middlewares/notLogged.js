81436c2e8cf089ef2a15c1c9ad44a403b56884d3
Washington Bruno <washingtonbruno1@gmail.com>
2019-02-25 01:32:21 -0300

    Adicionando middlewares para login

---
+++ src/router/middlewares/notLogged.js
@@ -0,0 +1,10 @@
+export default ({to, next, router}) => {
+  const token = window.localStorage.token || null
+  const user = window.localStorage.user || null
+
+  if (!token || !user) {
+    return router.push({ name: 'login' })
+  }
+
+  next()
+}
